fb18e603c4389210f69fab2ebd4f3aabb696f0eb
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-12-11 21:20:17 -0600

    CREATE CONFIG FOLDER W/CONNECTION.JS FILE
    - Added connection to the database in connection.js file. Now everyone should be able to use their own .ENV folder for sensitive info.

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
