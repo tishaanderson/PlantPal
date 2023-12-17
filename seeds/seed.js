148d5ca9ba493bc79e320d99d87a05375bad2d7c
Allex Ortiz <allexortiz@outlook.com>
2023-12-17 14:38:44 -0600

    various updates, see comments

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,43 @@
 const sequelize = require('../config/connection');
 const { Plant } = require('../models');
-
-
 const plantData = require('./plants.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  try {
+    await sequelize.sync({ force: true });
 
-  const plants = await Plant.bulkCreate(plantData, {
-    individualHooks: true,
-    returning: true,
-  });
+    const plants = await Plant.bulkCreate(plantData, {
+      individualHooks: true,
+      returning: true,
+    });
 
- 
-
-  process.exit(0);
+    console.log(`${plants.length} plants seeded successfully.`);
+  } catch (error) {
+    console.error('Error seeding database:', error);
+  } finally {
+    process.exit(0);
+  }
 };
 
 seedDatabase();
+
+// const sequelize = require('../config/connection');
+// const { Plant } = require('../models');
+
+
+// const plantData = require('./plants.json');
+
+// const seedDatabase = async () => {
+//   await sequelize.sync({ force: true });
+
+//   const plants = await Plant.bulkCreate(plantData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
+
+ 
+
+//   process.exit(0);
+// };
+
+// seedDatabase();
