80d607a7b6d4972ae75d929fc131a4bf291b6c89
scnowak <143449653+scnowak@users.noreply.github.com>
2023-12-17 16:50:49 -0600

    unfinished changes to package.json and homepage.handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,4 @@
 DROP DATABASE IF EXISTS plantPal_db;
 CREATE DATABASE plantPal_db;
+USE plantPal_db;
+-- added USE line above SCN
--- server.js
+++ server.js
@@ -1,18 +1,52 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const path = require('path');
+const app = express();
+// const plantsData = require('./seeds/plants.json');
+const plantsData = require('./plants.json');
+
+// Configure Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Set the views folder
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route
+app.get('/', (req, res) => {
+  // Render the 'homepage' template with the 'main' layout
+  res.render('homepage', { title: 'PlantPals', plants: plantsData });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+//commented out code below line 1  10:41
+
+// const path = require('path');
+// const express = require('express');
+const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({  });
+
+//commented out code above 10:41
 
 const sess = {
   secret: 'Super secret secret',
@@ -36,14 +70,23 @@ app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
+//SCN
+// app.engine('handlebars', hbs.engine({
+//   layoutDir: `${__dirname}/views/layouts`
+// }));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
-});
+
+// commented out code below 10:39p
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+// });
