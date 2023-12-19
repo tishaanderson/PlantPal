cc11a45bb1abdae491f6e9f9f492ed9c90b0c19b
Allex Ortiz <allexortiz@outlook.com>
2023-12-18 21:53:47 -0600

    cleaned up main.handlebars

--- controllers/api/plantRoutes.js
+++ controllers/api/plantRoutes.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const { Plant } = require("../../models");
 
 router.get("/", async (req, res) => {
-    try {
-        const plants = await Plant.findAll();
-        res.json(plants);
-    } catch (error) {
-        console.error("Error fetching and rendering plants:", error);
-        res.status(500).send("Internal Server Error");
-    }
+  try {
+    const plants = await Plant.findAll();
+    res.json(plants);
+  } catch (error) {
+    console.error("Error fetching and rendering plants:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
