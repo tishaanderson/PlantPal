57099a4d0403aa370fa221b4f1ccc9503640acc2
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-12-22 12:18:34 -0600

    deleted commented out stuff

--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,3 @@
-// const withAuth = (req, res, next) => {
-//     if (!req.session.logged_in) {
-//         res.redirect('/login');
-//     } else {
-//         next();
-//     }
-// };
-
-// module.exports = withAuth;
-
 const withAuth = (req, res, next) => {
     if (req.session.logged_in) {
         res.locals.logged_in = true;
